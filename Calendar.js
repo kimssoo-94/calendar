735615611fdb70ea3daa05ff4bbc68de03599606
kimssoo199940429 <57531119+kimssoo199940429@users.noreply.github.com>
2020-04-30 16:22:01 +0900

    calendar_2

--- Calendar.js
+++ Calendar.js
@@ -20,7 +20,7 @@ const init = {
     this.activeDate = d;
     return d;
   },
-  addZero: (num) => (num < 10) ? '0' + num : num,
+  addZero: (num) => (num < 10) ? '0' + num : num, //"월"을 두자릿수로 설정
   activeDTag: null,
   getIndex: function (node) {
     let index = 0;
@@ -50,15 +50,39 @@ function loadDate (date, dayIn) {
 function loadYYMM (fullDate) {
   let yy = fullDate.getFullYear();
   let mm = fullDate.getMonth();
+  let judgemm = init.addZero(mm+1);
+  let judgeJpYear = yy.toString().concat(judgemm);
+  let jp; //和暦
   let firstDay = init.getFirstDay(yy, mm);
   let lastDay = init.getLastDay(yy, mm);
-  let markToday;  // for marking today date
+  let markToday;  //현재 날짜
   if (mm === init.today.getMonth() && yy === init.today.getFullYear()) {
     markToday = init.today.getDate();
   }
 
+  //일본 연호
+  if (Number(judgeJpYear) > 201903){
+    jp = yy - 2018;
+    jp = '(令和' + jp + ')年';
+  }else if (yy > 1988) {//平成
+    jp = yy - 1988;
+    jp = '(平成' + jp + ')年';
+  }else if (yy > 1925) {//昭和
+    jp = yy - 1925;
+    jp = '(昭和' + jp + ')年';
+  }else if (yy > 1911) {//大正
+    jp = yy - 1911;
+    jp = '(大正' + jp + ')年';
+  }else if (yy > 1867) {//明治
+    jp = yy - 1867;
+    jp = '(明治' + jp + ')年';
+  }else{               //該当なし
+    jp = '';
+  }
+
   document.querySelector('.cal-month').textContent = init.monList[mm];
   document.querySelector('.cal-year').textContent = yy;
+  document.querySelector('.cal-jpYear').textContent = jp;
 
   let trtd = '';
   let startCount;
@@ -74,8 +98,13 @@ function loadYYMM (fullDate) {
       } else {
         let fullDate = yy + '.' + init.addZero(mm + 1) + '.' + init.addZero(countDay + 1);
         trtd += '<td class="day';
+        //토일 색깔 지정해주기
+        if(j==0) trtd += ' sun';
+        else if(j==6) trtd += ' sat';
+        else ;
         trtd += (markToday && markToday === countDay + 1) ? ' today" ' : '"';
-        trtd += ` data-date="${countDay + 1}" data-fdate="${fullDate}">`;
+        trtd += ` data-date="${countDay + 1}" data-fdate="${fullDate}"`;
+        trtd += '>';
       }
       trtd += (startCount) ? ++countDay : '';
       if (countDay === lastDay.getDate()) { 
@@ -103,6 +132,5 @@ calBody.addEventListener('click', (e) => {
     loadDate(day, e.target.cellIndex);
     e.target.classList.add('day-active');
     init.activeDTag = e.target;
-    //init.activeDate.setDate(day);
   }
 });
